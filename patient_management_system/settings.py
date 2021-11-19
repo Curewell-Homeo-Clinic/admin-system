afe8c90263b7fae77d2c00b155a60f55d176e59d
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-19 22:14:40 +0530

    added mongodb db connection

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -75,15 +75,16 @@ WSGI_APPLICATION = 'patient_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'curewellTestDatabase',
+		'CLIENT': {
+			'host': 'mongodb+srv://admin:Kj9xTCf41vp7vE6f@notjust.nedmi.mongodb.net/curewellTestDatabase?retryWrites=true&w=majority',
+		}
+		# 'HOST': 'mongodb+srv://admin:Kj9xTCf41vp7vE6f@notjust.nedmi.mongodb.net/curewellTestDatabase?retryWrites=true&w=majority',
+		# 'USERNAME': 'admin',
+		# 'PASSWORD': 'Kj9xTCf41vp7vE6f',
     }
 }
-
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
