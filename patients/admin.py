f949d4d74339786c7fc8a7fa491bedc4d87d9bd3
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-19 21:06:28 +0530

    chore: added filters

--- patients/admin.py
+++ patients/admin.py
@@ -5,3 +5,11 @@ from .models import Patient
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'age')
+    list_display_links = ('id', 'name')
+    list_filter = ('admitted_at', )
+    
+    def name(self, obj):
+        if obj.last_name:
+            return f'{ obj.first_name} { obj.last_name }'
+        else:
+            return obj.first_name
--- patients/models.py
+++ patients/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 class Patient(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
     phone_no = models.CharField(max_length=100)
     age = models.IntegerField()
+    admitted_at = models.DateField(auto_now_add=True, blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f'{ self.first_name } { self.last_name }'
