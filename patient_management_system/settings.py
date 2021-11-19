56a3a0849b209f9aca244975f3c74b64d9c03c3b
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-19 13:55:16 +0530

    fixed: whitenoise middleware

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
