60da93f772820d85652e10a9f6852f555d7cbca0
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-20 16:11:17 +0530

    feat:applied patient list view

--- patients/urls.py
+++ patients/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import index, PatientDetail, PatientList
 
 urlpatterns = [
-    path('', views.index, name='index'),
+	path('', index, name='index'),
+    path('patients/', PatientList.as_view(), name='patients'),
+	path('patient/<int:pk>/', PatientDetail.as_view(), name='patient'),
 ]
