83ef60cd1bf86cc3ab65fe9b79af7d6a37f29961
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-20 21:00:08 +0530

    chore: caching enabeld

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -66,18 +66,19 @@ DATABASES = {
 }
 
 # CACHE_TTL = 60 * 60 * 24 * 365
+CACHE_TTL = 60 * 1500
 
-# CACHES = {
-# 	'default': {
-# 		'BACKEND': 'django_redis.cache.RedisCache',
-# 		'LOCATION': 'redis://127.0.0.1:6379/1',
-# 		'OPTIONS': {
-# 			'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-# 		},
-# 		'KEY_PREFIX': 'curewell',
-# 		'TIMEOUT': CACHE_TTL,
-# 	}
-# }
+CACHES = {
+	'default': {
+		'BACKEND': 'django_redis.cache.RedisCache',
+		'LOCATION': 'redis://127.0.0.1:6379/1',
+		'OPTIONS': {
+			'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+		},
+		'KEY_PREFIX': 'curewell',
+		'TIMEOUT': CACHE_TTL,
+	}
+}
 
 # Password validation
 
--- patients/views.py
+++ patients/views.py
@@ -1,14 +1,47 @@
+from patient_management_system.settings import CACHE_TTL
 from .models import Patient
 from django.shortcuts import redirect, render
+from django.conf import settings
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.core.cache import cache
 
-def index(_request):
+CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
+
+def index(request):
 	response = redirect('/patients/')
 	return response
 
+def get_patient(request, filter_patient = None):
+	if filter_patient:
+		print("DATA COMING FROM DB")
+		patients = Patient.objects.filter(first_name__icontains=filter_patient)
+	else:
+		patients = Patient.objects.all()
+
+	return patients
+
 def patient_list(request):
-	patients = Patient.objects.all()
-	return render(request, 'patients/patient_list.html', {'patients': patients})
+	filter_patient = request.GET.get('filter_patient')
+	if cache.get(filter_patient):
+		patients = cache.get(filter_patient)
+	else:
+		if filter_patient:
+			patients = get_patient(request, filter_patient)
+			cache.set(filter_patient, patients)
+		else:
+			patients = get_patient(request)
+	
+	context = { 'patients': patients }
+	return render(request, 'patients/patient_list.html', context=context)
 
 def patient_detail(request, pk):
-	patient = Patient.objects.get(pk=pk)
-	return render(request, 'patients/patient_detail.html', {'patient': patient})
+	if cache.get(pk):
+		print("DATA COMING FROM CACHE")
+		patient = cache.get(pk)
+	else:
+		print("DATA COMING FROM DB")
+		patient = Patient.objects.get(pk=pk)
+		cache.set(pk, patient)
+	
+	context = { 'patient': patient }
+	return render(request, 'patients/patient_detail.html', context)
