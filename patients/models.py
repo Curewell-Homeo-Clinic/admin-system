e7af67295cf17cd63852b3bc0a0da16be66a05dc
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-20 21:35:18 +0530

    chrore made phone no only 10 characters long

--- patients/models.py
+++ patients/models.py
@@ -5,7 +5,7 @@ class Patient(models.Model):
     id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100, blank=True, null=True)
-    phone_no = models.CharField(max_length=100)
+    phone_no = models.CharField(max_length=10, blank=True, null=True)
     age = models.IntegerField()
     admitted_at = models.DateField(auto_now_add=True, blank=True, null=True)
 
