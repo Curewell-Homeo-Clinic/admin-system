5207d2f50cadcf4b7505afee54795e268e6c903f
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-21 11:43:15 +0530

    feat: added basic auth to patient data pages

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = '((#ub(l909_r6)_@ezsggi-2%d8h+3@u^!e6p57tcb#g@05e=+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['curewell-admin.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['curewell-admin.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
