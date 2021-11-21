de1a4e8023619a96f4f3ec0a6e0bd4a50e3d184a
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-21 22:30:42 +0530

    feat: added doctor list and details

--- patients/urls.py
+++ patients/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import appointment_detail, appointment_list, dashboard, patient_list, patient_detail
+from .views import appointment_detail, appointment_list, dashboard, doctor_detail, doctor_list, patient_list, patient_detail
 
 urlpatterns = [
     path('', dashboard, name='index'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('patient/<int:pk>/', patient_detail, name='patient'),
     path('appointments/', appointment_list, name="appointments"),
     path('appointment/<int:pk>/', appointment_detail, name="appointment"),
+    path('doctors/', doctor_list, name='doctors'),
+    path('doctor/<int:pk>/', doctor_detail, name='doctor'),
 ]
--- patients/views.py
+++ patients/views.py
@@ -74,6 +74,49 @@ def patient_detail(request, pk):
     return render(request, 'patients/patient_detail.html', context)
 
 
+def get_doctor(filter_doctor=None):
+    if filter_doctor:
+        doctors = Doctor.objects.filter(first_name__icontains=filter_doctor)
+    else:
+        doctors = Doctor.objects.all()
+
+    return doctors
+
+
+@login_required(login_url='/admin/login/')
+def doctor_list(request):
+    filter_doctor = request.GET.get('filter_doctor')
+    if cache.get(f'doctor_{filter_doctor}'):
+        doctors = cache.get(f'doctor_{filter_doctor}')
+    else:
+        if filter_doctor:
+            doctors = get_doctor(filter_doctor)
+            cache.set(f'doctor_{filter_doctor}', doctors, CACHE_TTL)
+        else:
+            doctors = get_doctor()
+
+    context = {'doctors': doctors, 'active': 'doctor'}
+    return render(request, 'doctors/doctor_list.html', context=context)
+
+
+@login_required(login_url='/admin/login/')
+def doctor_detail(request, pk):
+    if cache.get(f'doctor_{pk}'):
+        doctor = cache.get(f'doctor_{pk}')
+    else:
+        doctor = Doctor.objects.get(pk=pk)
+        cache.set(f'doctor_{pk}', doctor, CACHE_TTL)
+
+    context = {
+        'doctor': doctor,
+        'doctor_edit': f'/admin/patients/doctor/{doctor.id}/change',
+        'doctor_delete': f'/admin/patients/doctor/{doctor.id}/delete',
+        'active': 'doctor'
+    }
+
+    return render(request, 'doctors/doctor_detail.html', context)
+
+
 def get_appointment(patient_name=None):
     if patient_name:
         patient = Patient.objects.filter(first_name__icontains=patient_name)
