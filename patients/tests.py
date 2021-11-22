c3b8c7d34ae9a678f2147ac917742001906a534d
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-22 15:06:31 +0530

    feat: wrote tests for user, patient, doctor and patient

--- patients/tests.py
+++ patients/tests.py
@@ -1,23 +1,104 @@
+from datetime import date
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.contrib.auth.models import User
+
+from patients.models import Patient, Doctor, Appointment
+
+User = get_user_model()
 
 
-# Create your tests here.
 class UserTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser',
-                                             password='asfasfgadgs')
+                                             email="testuser@invalid.com",
+                                             password='asdasd')
         self.user.save()
 
-    def test_user_created(self):
+    def test_user_exists(self):
+        self.assertEqual(User.objects.all().count(), 1)
+
+    def test_user_authentication(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_password(self):
+        self.assertEqual(self.user.check_password('asdasd'), True)
+
+    def test_user_props(self):
         self.assertEqual(self.user.username, 'testuser')
-        self.assertEqual(self.user.is_active, True)
+        self.assertEqual(self.user.email, 'testuser@invalid.com')
         self.assertEqual(self.user.is_staff, False)
         self.assertEqual(self.user.is_superuser, False)
-        self.assertEqual(self.user.is_authenticated, True)
-        self.assertEqual(self.user.email, '')
-        self.assertEqual(self.user.check_password('asfasfgadgs'), True)
-        self.assertEqual(self.user.check_password('asfasasdasfgadg'), False)
-        self.assertEqual(self.user.groups.count(), 0)
 
-        self.user.delete()
+
+class PatientTestCase(TestCase):
+    def setUp(self):
+        self.patient = Patient.objects.create(first_name='test',
+                                              last_name='user',
+                                              phone_no='1234567890',
+                                              age='37')
+        self.patient.save()
+
+    def test_patient_exists(self):
+        self.assertEqual(Patient.objects.all().count(), 1)
+
+    def test_patient_props(self):
+        self.assertEqual(self.patient.first_name, 'test')
+        self.assertEqual(self.patient.last_name, 'user')
+        self.assertEqual(self.patient.phone_no, '1234567890')
+        self.assertEqual(self.patient.age, '37')
+        self.assertEqual(self.patient.admitted_at, date.today())
+
+
+class DoctorTestCase(TestCase):
+    def setUp(self):
+        self.doctor = Doctor.objects.create(first_name='test',
+                                            last_name='user',
+                                            phone_no='1234567890',
+                                            age='37',
+                                            email='testuser@invalid.com')
+        self.doctor.save()
+
+    def test_doctor_exists(self):
+        self.assertEqual(Doctor.objects.all().count(), 1)
+
+    def test_doctor_props(self):
+        self.assertEqual(self.doctor.first_name, 'test')
+        self.assertEqual(self.doctor.last_name, 'user')
+        self.assertEqual(self.doctor.phone_no, '1234567890')
+        self.assertEqual(self.doctor.age, '37')
+        self.assertEqual(self.doctor.email, 'testuser@invalid.com')
+
+
+class AppointmentTestCase(TestCase):
+    def setUp(self):
+        self.patient = Patient.objects.create(first_name='test',
+                                              last_name='user',
+                                              phone_no='1234567890',
+                                              age='37')
+
+        self.doctor = Doctor.objects.create(first_name='test',
+                                            last_name='user',
+                                            phone_no='1234567890',
+                                            age='37',
+                                            email='testuser@invalid.com')
+
+        self.appointment = Appointment.objects.create(patient=self.patient,
+                                                      doctor=self.doctor,
+                                                      date='2017-01-01',
+                                                      time='10:00')
+
+        self.patient.save()
+        self.doctor.save()
+        self.appointment.save()
+
+    def test_appointment_exists(self):
+        self.assertEqual(Patient.objects.all().count(), 1)
+        self.assertEqual(Doctor.objects.all().count(), 1)
+        self.assertEqual(Appointment.objects.all().count(), 1)
+
+    def test_appointment_props(self):
+        self.assertEqual(self.appointment.patient, self.patient)
+        self.assertEqual(self.appointment.doctor, self.doctor)
+        self.assertEqual(self.appointment.date, '2017-01-01')
+        self.assertEqual(self.appointment.time, '10:00')
