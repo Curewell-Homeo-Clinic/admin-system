78133b40178241da624cc5a6bd29304aeffc5370
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-25 12:19:01 +0530

    feat: configured redis enterprise cloud server

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -91,6 +91,7 @@ CACHES = {
         'BACKEND': 'django_redis.cache.RedisCache',
         'LOCATION': config('REDIS_URL'),
         'OPTIONS': {
+			'PASSWORD': config('REDIS_PASSWORD'),
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
         'KEY_PREFIX': 'curewell',
