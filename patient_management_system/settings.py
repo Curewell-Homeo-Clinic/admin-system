f06132243ebf348a281b63d44ea12a9166c69493
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-11-25 10:50:57 +0530

    chore: updated for heroku redis

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -89,7 +89,7 @@ CACHE_TTL = 60 * 1500
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': config('REDIS_URL'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
