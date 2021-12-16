7c286518c6ef0bb0ccbede9e99728d36cdf02ce2
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-12-16 16:56:27 +0530

    feature: added yearly sales graph

--- patients/urls.py
+++ patients/urls.py
@@ -1,19 +1,9 @@
 from django.urls import path
 
-from .views import (
-	appointment_detail, 
-	appointment_list, 
-	dashboard, 
-	doctor_detail, 
-	doctor_list, 
-	patient_list, 
-	patient_detail,
-	invoice_list,
-	invoice_detail,
-	invoice_print,
-	get_data,
-	stats
-)
+from .views import (appointment_detail, appointment_list, dashboard,
+                    doctor_detail, doctor_list, patient_list, patient_detail,
+                    invoice_list, invoice_detail, invoice_print,
+                    get_sales_chart, stats)
 
 urlpatterns = [
     path('', dashboard, name='index'),
@@ -23,9 +13,10 @@ urlpatterns = [
     path('appointment/<int:pk>/', appointment_detail, name="appointment"),
     path('doctors/', doctor_list, name='doctors'),
     path('doctor/<int:pk>/', doctor_detail, name='doctor'),
-	path('invoices/', invoice_list, name='invoices'),
-	path('invoice/<int:pk>/', invoice_detail, name='invoice'),
-	path('invoice/<int:pk>/print/', invoice_print, name='invoice_print'),
-	path('stats/', stats, name='stats'),
-	path('api/v1/get_stats', get_data, name='api__get_stats')
+    path('invoices/', invoice_list, name='invoices'),
+    path('invoice/<int:pk>/', invoice_detail, name='invoice'),
+    path('invoice/<int:pk>/print/', invoice_print, name='invoice_print'),
+    path('stats/', stats, name='stats'),
+    path('api/v1/get_sales/<int:year>', get_sales_chart,
+         name='api__get_sales'),
 ]
--- static/js/stats.js
+++ static/js/stats.js
@@ -1,1 +1,78 @@
-console.log('stats')
+const ctx = document.getElementById("sales_chart").getContext("2d");
+
+// Gradient Fill
+let gradient = ctx.createLinearGradient(0, 0, 0, 400);
+gradient.addColorStop(0, "#51e6c4");
+gradient.addColorStop(1, "#25da91");
+
+let delayed;
+
+function addCurrencySign(value) {
+  if (value == 0) {
+    return value;
+  }
+  return "₹" + value;
+}
+
+let animation = {
+  onComplete: () => {
+    delayed = 0;
+  },
+  delay: (context) => {
+    let delay = 0;
+    if (context.type === "data" && context.mode == "default" && !delayed) {
+      delay = context.dataIndex * 300 + context.datasetIndex * 100;
+    }
+
+    return delay;
+  },
+};
+
+function countOccurences(arr, val) {
+  return arr.reduce((a, v) => (v === val ? a + 1 : a), 0);
+}
+
+(async function createYearChart() {
+  const year = new Date().getFullYear();
+  const salesData = await (await fetch(`/api/v1/get_sales/${year}`)).json();
+
+  const data = {
+    labels: salesData.labels,
+    datasets: [
+      {
+        label: `Sales ${year}`,
+        backgroundColor: gradient,
+        pointBackgroundColor: "#87e4b6",
+        tension: 0.4,
+        fill: true,
+        data: salesData.data,
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      radius: 3,
+      hoverRadius: 5,
+      hitRadius: 30,
+      responsive: true,
+      animation,
+      scales: {
+        y: {
+          ticks: {
+            callback: (value) => addCurrencySign(value),
+          },
+        },
+      },
+    },
+  };
+
+  // don't show the animation when the no of 0 sales is greater than or equal to 6.
+  if (countOccurences(salesData.data, 0) >= 6) {
+    delete config.options.animation;
+  }
+
+  const sales_chart = new Chart(ctx, config);
+})();
