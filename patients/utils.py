917c52c871ad59403c0cb72e5a1893d5d6eb213b
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-12-17 08:17:03 +0530

    completed dashboard context

--- patients/utils.py
+++ patients/utils.py
@@ -2,7 +2,11 @@ import calendar
 from datetime import date, datetime
 from django.db.models import Sum
 
-from .models import Invoice
+from .models import (
+    Invoice,
+    Appointment,
+    Patient,
+)
 
 
 def get_current_month_sales():
@@ -13,6 +17,52 @@ def get_current_month_sales():
 
     return Invoice.objects.filter(date__range=[first_day, last_day]).aggregate(
         Sum('total_fee'))['total_fee__sum']
+
+
+def get_current_month_appointments():
+    today = date.today()
+    _, num_days = calendar.monthrange(today.year, today.month)
+    first_day = date(today.year, today.month, 1)
+    last_day = date(today.year, today.month, num_days)
+
+    return Appointment.objects.filter(
+        date__range=[first_day, last_day]).count()
+
+
+def get_current_month_patients():
+    today = date.today()
+    _, num_days = calendar.monthrange(today.year, today.month)
+    first_day = date(today.year, today.month, 1)
+    last_day = date(today.year, today.month, num_days)
+
+    return Patient.objects.filter(
+        admitted_at__range=[first_day, last_day]).count()
+
+
+def get_current_month_stats() -> dict:
+    return {
+        'sales': get_current_month_sales(),
+        'appointments': get_current_month_appointments(),
+        'patients': get_current_month_patients(),
+    }
+
+
+def get_gross_stats() -> dict:
+    return {
+        'patients': Patient.objects.all().count(),
+        'appointments': Appointment.objects.all().count(),
+        'sales': Invoice.objects.all().count(),
+    }
+
+
+def get_current_year_stats() -> dict:
+    year = date.today().year
+
+    return {
+        'patients': Patient.objects.filter(admitted_at__year=year).count(),
+        'appointments': Appointment.objects.filter(date__year=year).count(),
+        'sales': Invoice.objects.filter(date__year=year).count(),
+    }
 
 
 MONTHS = [
