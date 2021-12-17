e547ea6c6db603fb6b5981fae766c81c2b9c0ee3
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-12-17 10:40:28 +0530

    refactor: changed the money formatter for ruppes

--- static/js/format_ammount.js
+++ static/js/format_ammount.js
@@ -1,5 +1,9 @@
 for (let i = 0; i < document.getElementsByClassName("money").length; i++) {
   let money_elm = document.getElementsByClassName("money")[i];
   let money_elm_value = money_elm.innerHTML;
-  money_elm.innerHTML = money_elm_value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  money_elm.innerHTML = `&#8377;${format_ammount(parseInt(money_elm_value))}`;
+}
+
+function format_ammount(number) {
+  return Intl.NumberFormat("en-IN").format(number);
 }
