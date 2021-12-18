687dbbd81e02257af99edb7e2bab2ae7e2183a93
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-12-18 10:56:09 +0530

    feat: added gzip middleware to improve performance

--- patient_management_system/settings.py
+++ patient_management_system/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.gzip.GZipMiddleware'
 ]
 
 ROOT_URLCONF = 'patient_management_system.urls'
