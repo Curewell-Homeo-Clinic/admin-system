9cbe81cc75f27d3e0f0926d0d09d57da916f250d
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2021-12-21 11:02:11 +0530

    chore: enable sentry only in production

--- patient_management_system/settings/sentry.py
+++ patient_management_system/settings/sentry.py
@@ -2,7 +2,12 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from decouple import config
 
-sentry_sdk.init(dsn=config('SENTRY_DSN'),
-                integrations=[DjangoIntegration()],
-                traces_sample_rate=1.0,
-                send_default_pii=True)
+from .settings import DEBUG
+
+if DEBUG == False:
+    print("Sentry is enabled")
+    sentry_sdk.init(environment='PRODUCTION',
+                    dsn=config('SENTRY_DSN'),
+                    integrations=[DjangoIntegration()],
+                    traces_sample_rate=1.0,
+                    send_default_pii=True)
