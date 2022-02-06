1705b69c49a142d0ba0045e185ca21447c731b11
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-02-06 12:57:18 +0530

    Revert "Containerize Django Application"

--- patient_management_system/settings/sentry.py
+++ patient_management_system/settings/sentry.py
@@ -5,6 +5,7 @@ from decouple import config
 from .settings import DEBUG
 
 if DEBUG == False:
+    print("Sentry is enabled")
     sentry_sdk.init(environment='PRODUCTION',
                     dsn=config('SENTRY_DSN'),
                     integrations=[DjangoIntegration()],
